1f68210d9ad3f3a768afd5fa9e1b9c7f66e198a4
Loa212 <anobileloris@gmail.com>
2022-12-27 22:30:36 +0100

    theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#BF32E4",
+        secondary: "#14E9F1",
+        light: "#EDF7F6",
+        dark: "#0A090C",
+      },
+    },
+  },
+  daisyui: {
+    themes: [],
   },
   plugins: [require("daisyui")],
 };
