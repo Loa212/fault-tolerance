0e4c9ce6c402fed777163059363256a8161acd6a
Loa212 <anobileloris@gmail.com>
2023-02-16 21:02:40 +0100

    change testimonials cards

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 import Close from "./Close.astro";
 import CheckCircle from "./CheckCircle.astro";
+import LogoGithub from "./LogoGithub.astro";
 
-export { Close, CheckCircle };
+export { Close, CheckCircle, LogoGithub };
