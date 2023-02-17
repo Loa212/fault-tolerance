bd4c427daf37250d64f542617281b87358a62bb8
Loa212 <anobileloris@gmail.com>
2023-02-17 19:24:58 +0100

    fix build error

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,5 @@
 import Close from "./Close.astro";
 import CheckCircle from "./CheckCircle.astro";
 import LogoGithub from "./LogoGithub.astro";
-import ArrowCircleUp from "./ArrowCircleUp.astro";
 
-export { Close, CheckCircle, LogoGithub, ArrowCircleUp };
+export { Close, CheckCircle, LogoGithub };
