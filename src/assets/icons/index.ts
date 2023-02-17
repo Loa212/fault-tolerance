9493270b6a00b47d41762555a497ca819e2ecee6
Loa212 <anobileloris@gmail.com>
2023-02-17 19:18:15 +0100

    anchor links

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,6 @@
 import Close from "./Close.astro";
 import CheckCircle from "./CheckCircle.astro";
 import LogoGithub from "./LogoGithub.astro";
+import ArrowCircleUp from "./ArrowCircleUp.astro";
 
-export { Close, CheckCircle, LogoGithub };
+export { Close, CheckCircle, LogoGithub, ArrowCircleUp };
